5c74957f32a266ebcf8d510cbdcc18db1850f4a5
Hayk Tunyan <hayktunyan1994@gmail.com>
2026-07-25 18:45:51 +0400

    add the sport folder

--- src/container/your-city/YourCityClient.tsx
+++ src/container/your-city/YourCityClient.tsx
@@ -4,13 +4,13 @@ import React, { useState } from 'react';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
-import AgarakFC from "#/images/football/Agarak.png";
-import GorisFC from "#/images/football/Goris.png";
-import KapanFC from "#/images/football/Kapan.png";
-import SisianFC from "#/images/football/Sisian.png";
-import MeghriFC from "#/images/football/Meghri.png";
-import KajaranFC from "#/images/football/Kajaran.png";
-import DastakertFC from "#/images/football/Dastakert.png";
+import AgarakFC from "#/images/sport/agar.png";
+import GorisFC from "#/images/sport/goris.png";
+import KapanFC from "#/images/sport/kapan.png";
+import SisianFC from "#/images/sport/sisian.png";
+import MeghriFC from "#/images/sport/meghri.png";
+import KajaranFC from "#/images/sport/kajaran.png";
+import DastakertFC from "#/images/sport/dastakert.png";
 
 const CLUBS = [
   {
